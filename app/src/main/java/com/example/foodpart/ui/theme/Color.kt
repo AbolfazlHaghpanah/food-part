b50ab6ac0299d7ebc6dce7010220cfa9b6083755
Abolfazl Haghpanah <abolfazlhaghpanah77@gmail.com>
2023-08-30 19:01:08 +0330

    set Themes

--- app/src/main/java/com/example/foodpart/ui/theme/Color.kt
+++ app/src/main/java/com/example/foodpart/ui/theme/Color.kt
@@ -2,10 +2,12 @@ package com.example.foodpart.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val primary = Color(0xFFFF6262)
+val secondary = Color(0xFF273940)
+val background = Color(0xFF17171A)
+val surface = Color(0xFF222228)
+val onBackground =  Color(0xFFF8F8F8)
+val onSurface = Color(0xFFE6E6E6)
+val green = Color(0xFF00FF67)
+val red = Color(0xFFFF4444)
+val yellow = Color(0xFFFFE100)
--- app/src/main/java/com/example/foodpart/ui/theme/Type.kt
+++ app/src/main/java/com/example/foodpart/ui/theme/Type.kt
@@ -1,34 +1,43 @@
 package com.example.foodpart.ui.theme
 
-import androidx.compose.material3.Typography
+import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.foodpart.R
 
-// Set of Material typography styles to start with
+
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+    h1 = TextStyle(
+        fontSize = 24.sp,
+        fontFamily = FontFamily(Font(R.font.iranyekan_bold)),
+        fontWeight = FontWeight(800),
+        letterSpacing = 0.36.sp,
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
+    h2 = TextStyle(
+        fontSize = 20.sp,
+        fontFamily = FontFamily(Font(R.font.iranyekan_bold)),
+        fontWeight = FontWeight(800),
+        letterSpacing = 0.3.sp,
+    ),
+    h3 = TextStyle(
+        fontSize = 14.sp,
+        fontFamily = FontFamily(Font(R.font.iranyekan_regular)),
+        fontWeight = FontWeight(700),
+    ),
+    subtitle1 = TextStyle(
+        fontSize = 13.sp,
+        lineHeight = 19.8.sp,
+        fontFamily = FontFamily(Font(R.font.iranyekan_regular)),
+        fontWeight = FontWeight(400),
+    ),
+    subtitle2 = TextStyle(
         fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        lineHeight = 16.7.sp,
+        fontFamily = FontFamily(Font(R.font.iranyekan_mediom)),
+        fontWeight = FontWeight(400),
     )
-    */
+
 )
