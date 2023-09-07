fb7e99face2be2b09136f095968a39ecb2edfa50
Abolfazl Haghpanah <abolfazlhaghpanah77@gmail.com>
2023-09-08 01:15:31 +0330

    add on value change to FoodPartTextField

--- app/src/main/java/com/example/foodpart/ui/components/FoodPartTextField.kt
+++ app/src/main/java/com/example/foodpart/ui/components/FoodPartTextField.kt
@@ -3,7 +3,6 @@ package com.example.foodpart.ui.components
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedTextField
@@ -12,7 +11,6 @@ import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 
 @Composable
