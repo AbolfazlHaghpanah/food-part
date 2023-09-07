486d910413fcf2393aeb40f65e43013cfa756f65
Abolfazl Haghpanah <abolfazlhaghpanah77@gmail.com>
2023-09-08 01:15:23 +0330

    add on value change to FoodPartTextField

--- app/src/main/java/com/example/foodpart/ui/components/FoodPartTextField.kt
+++ app/src/main/java/com/example/foodpart/ui/components/FoodPartTextField.kt
@@ -3,6 +3,7 @@ package com.example.foodpart.ui.components
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedTextField
@@ -11,6 +12,7 @@ import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 
 @Composable
@@ -18,13 +20,14 @@ fun FoodPartTextField(
     textFieldState: MutableState<String>,
     label: String,
     modifier: Modifier = Modifier,
-    placeholderCND :String? = null,
-    keyboardOptions: KeyboardOptions = KeyboardOptions()
+    onValueChange: (String) -> Unit = { it -> textFieldState.value = it },
+    placeholderCND: String? = null,
+    keyboardOptions: KeyboardOptions = KeyboardOptions(),
 ) {
     OutlinedTextField(
         keyboardOptions = keyboardOptions,
         value = textFieldState.value,
-        onValueChange = { it -> textFieldState.value = it },
+        onValueChange = onValueChange,
         modifier = modifier
             .fillMaxWidth(),
         placeholder = {
@@ -33,8 +36,10 @@ fun FoodPartTextField(
                     text = label,
                     style = MaterialTheme.typography.body1
                 )
-                Spacer(modifier = Modifier
-                    .weight(1F))
+                Spacer(
+                    modifier = Modifier
+                        .weight(1F)
+                )
                 Text(
                     text = placeholderCND.orEmpty(),
                     style = MaterialTheme.typography.body1
@@ -53,7 +58,8 @@ fun FoodPartTextField(
         textStyle = MaterialTheme
             .typography
             .body1
-            .copy(textAlign = TextAlign.Start)
-    )
+            .copy(textAlign = TextAlign.Start),
+
+        )
 
 }
